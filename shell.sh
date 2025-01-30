e9425884ef609f983f456c4d440d5a4094ddd63a
damengrandom <damonmaozewu@gmail.com>
2025-01-30 21:18:44 +1100

    ckad memos init

---
+++ shell.sh
@@ -0,0 +1,11 @@
+# Common commands
+
+# head -c 32 /dev/urandom | base64 # generate 32 characters random string
+# usage: used for generating a random secret string !!!!
+
+# echo "SECREST_HASH=" | base64 --decode # decode a secret !!
+
+# cat /etc/*release* # check what os on the machines?
+
+# ps -ef | grep kube-apiserver | grep admission-plugins # To see all admissions
+
